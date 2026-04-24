c10dc71c1beeece7ba7125b10982be16012d82ee
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 00:22:05 +0300

    feat: add EF Core config for room

---
+++ MeetingRoomsBooking/Infrastructure/Data/Configurations/RoomsEntity/RoomsConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeetingRoomsBooking.Infrastructure.Data.Configurations.RoomsEntity
+{
+    public sealed class RoomsConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> entity)
+        {
+            entity.HasKey(r => r.Id);
+            entity.Property(r => r.Id).ValueGeneratedOnAdd();
+
+            entity.ConfigureValueObjects();
+        }
+    }
+}
---
+++ MeetingRoomsBooking/Infrastructure/Data/Configurations/RoomsEntity/RoomsConfigurationExtension.cs
@@ -0,0 +1,34 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Domain.Entities;
+using MeetingRoomsBooking.Features.Rooms.Domain.Ids;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomCapacity;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomLocation;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomName;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeetingRoomsBooking.Infrastructure.Data.Configurations.RoomsEntity
+{
+    public static class RoomsConfigurationExtension
+    {
+        public static void ConfigureValueObjects(this EntityTypeBuilder<Room> entity)
+        {
+            entity.Property(r => r.Id)
+                .HasConversion(rId => rId.Value, value => RoomId.FromDatabase(value))
+                .IsRequired();
+
+            entity.Property(r => r.Name)
+                .HasConversion(name => name.Value, value => RoomName.Create(value))
+                .IsRequired();
+
+            entity.Property(r => r.Capacity)
+                .HasConversion(capacity => capacity.Value, value => RoomCapacity.Create(value))
+                .IsRequired();
+
+            entity.Property(r => r.Location)
+                .HasConversion(location => location.Value, value => RoomLocation.Create(value))
+                .IsRequired();
+
+            entity.Property(r => r.IsActive)
+                .IsRequired();
+        }
+    }
+}
