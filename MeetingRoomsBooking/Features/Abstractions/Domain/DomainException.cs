01a633cec4673bc0d7eaea9ae446f3dc64d77743
Oleg Boodko <dafsas321@gmail.com>
2026-04-24 23:44:35 +0300

    task: add base domain exception

---
+++ MeetingRoomsBooking/Features/Abstractions/Domain/DomainException.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace MeetingRoomsBooking.Features.Abstractions.Domain
+{
+    public abstract class DomainException : Exception
+    {
+        public string Code { get; }
+        public IReadOnlyDictionary<string, object?> Meta {  get; }
+
+        protected DomainException(
+            string code,
+            string message,
+            IDictionary<string, object?>? meta = null) : base(message)
+        {
+            Code = code;
+            Meta = meta is null
+                ? new Dictionary<string, object?>()
+                : new Dictionary<string, object?>(meta);
+        }
+    }
+}
