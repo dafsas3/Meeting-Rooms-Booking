7498c3102d62753c1b82f021a23f03d6ec77b96c
Oleg Boodko <dafsas321@gmail.com>
2026-04-24 23:46:28 +0300

    feat: configure booking database context

---
+++ MeetingRoomsBooking/Infrastructure/Data/BookingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomsBooking.Infrastructure.Data
+{
+    public sealed class BookingDbContext : DbContext
+    {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) : 
+            base(options) { }
+
+        public DbSet<Room> Rooms => Set<Room>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookingDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
