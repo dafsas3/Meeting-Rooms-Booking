35c079db4cbe0ebc75d89de896ec919523da672c
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 15:30:57 +0300

    feat: Added extension for getting unique index name in case of DB error

---
+++ MeetingRoomsBooking/Infrastructure/Persistence/DbExtensions/DbUpdateExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.DbExtensions
+{
+    public static class DbUpdateExceptionExnetsions
+    {
+        public static bool TryGetDuplicateName(this DbUpdateException ex, out string? constraintName)
+        {
+            constraintName = null;
+
+            if (ex.InnerException is not PostgresException pEx) return false;
+
+            if(pEx.SqlState is not PostgresErrorCodes.UniqueViolation) return false;
+
+            constraintName = pEx.ConstraintName;
+            return true;
+        }
+    }
+}
