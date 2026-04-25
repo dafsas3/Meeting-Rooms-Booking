23ba267a2309d71c8e7edcb20a7a0edad1636278
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 13:54:02 +0300

    feat: Added queries to the DB for reading data with layer separation

---
+++ MeetingRoomsBooking/Features/Rooms/Application/Abstractions/Queries/IRoomQueries.cs
@@ -0,0 +1,9 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomName;
+
+namespace MeetingRoomsBooking.Features.Rooms.Application.Abstractions.Queries
+{
+    public interface IRoomQueries
+    {
+        Task<bool> IsExistsByNameAsync(RoomName name, CancellationToken ct);
+    }
+}
---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Queries/EfRoomQueries.cs
@@ -0,0 +1,19 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Application.Abstractions.Queries;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomName;
+using MeetingRoomsBooking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Queries
+{
+    public class EfRoomQueries : IRoomQueries
+    {
+        private readonly BookingDbContext _db;
+
+        public EfRoomQueries(BookingDbContext db) => _db = db;
+
+
+        public async Task<bool> IsExistsByNameAsync(RoomName name, CancellationToken ct)
+            => await _db.Rooms
+            .AnyAsync(r => r.Name == name, ct);
+    }
+}
