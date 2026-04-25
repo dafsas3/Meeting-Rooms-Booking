64bd535f3ff2ee70142fd1d261a5947fa874dcb4
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 16:22:28 +0300

    fix: room errors

--- MeetingRoomsBooking/Features/Rooms/Application/Errors/RoomError.cs
+++ MeetingRoomsBooking/Features/Rooms/Application/Errors/RoomError.cs
@@ -4,7 +4,8 @@ namespace MeetingRoomsBooking.Features.Rooms.Application.Errors
 {
     public static class RoomError
     {
-        public static ApiError NameAlreadyExists(string value)
-            => new("ROOM_NAME_ALREADY_EXISTS", $"The room name is already exists: {value}.");
+        public static ApiError NameAlreadyExistsInThisLocation(string room, string location)
+            => new(Code: "ROOM_NAME_IN_THE_LOCATION_ALREADY_EXISTS",
+                Message: $"The name already exists in this location: {room}, location: {location}");
     }
 }
