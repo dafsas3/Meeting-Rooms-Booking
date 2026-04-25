b75e1cd84750e334a0b74e9d02bea9ba6058ed5c
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 13:49:52 +0300

    feat: mapper Domain exception Code to http StatusCode

---
+++ MeetingRoomsBooking/Api/Problems/DomainErrorStatusMapper.cs
@@ -0,0 +1,18 @@
+﻿namespace MeetingRoomsBooking.Api.Problems
+{
+    public static class DomainErrorStatusMapper
+    {
+        public static int Map(string code)
+            =>
+            code switch
+            {
+                "INVALID_ROOM_CAPACITY" => StatusCodes.Status400BadRequest,
+                "INVALID_ROOM_LOCATION" => StatusCodes.Status400BadRequest,
+                "INVALID_ROOM_NAME" => StatusCodes.Status400BadRequest,
+                "INVALID_ROOM_ID" => StatusCodes.Status400BadRequest,
+                "ROOM_NAME_ALREADY_EXISTS" => StatusCodes.Status409Conflict,
+
+                _ => StatusCodes.Status400BadRequest
+            };
+    }
+}
