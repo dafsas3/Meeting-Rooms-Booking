dd6aad900446f1c8c043cbf72a9c60e7679bdf7e
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 15:32:25 +0300

    feat: Added a repository for writing to the rooms database

---
+++ MeetingRoomsBooking/Features/Rooms/Application/Abstractions/Repositories/IRoomRepository.cs
@@ -0,0 +1,9 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Domain.Entities;
+
+namespace MeetingRoomsBooking.Features.Rooms.Application.Abstractions.Repositories
+{
+    public interface IRoomRepository
+    {
+        void Add(Room entity);
+    }
+}
