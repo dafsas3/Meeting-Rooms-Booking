c565323c45f2002acc54622ab92dd2edde8c66f0
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 23:17:14 +0300

    feat: Added a structure for storing reusable domain-specific objects

---
+++ MeetingRoomsBooking/BuildingBlocks/Domain/Room/RoomId/RoomId.cs
@@ -0,0 +1,19 @@
+﻿namespace MeetingRoomsBooking.BuildingBlocks.Domain.Room.RoomId
+{
+    public readonly record struct RoomId
+    {
+        public int Value { get; }
+
+        private RoomId(int value) => Value = value;
+
+        public static RoomId Create(int value)
+        {
+            if (value <= 0)
+                throw InvalidRoomIdException.MustBeGreaterThanZero(value);
+
+            return new RoomId(value);
+        }
+
+        public static RoomId FromDatabase(int value) => new(value);
+    }
+}
