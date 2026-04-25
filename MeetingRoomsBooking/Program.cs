d13d5a7d5ce85ded78140ac614663dddd51fa821
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 13:55:35 +0300

    feat: added Middleware call

--- MeetingRoomsBooking/Program.cs
+++ MeetingRoomsBooking/Program.cs
@@ -1,3 +1,4 @@
+using MeetingRoomsBooking.Api.Middleware;
 using MeetingRoomsBooking.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseAuthorization();
 
