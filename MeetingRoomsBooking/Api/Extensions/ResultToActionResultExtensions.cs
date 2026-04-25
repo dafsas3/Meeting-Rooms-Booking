8fce6e42e542b8de7f2784983e7b6f1ba672023d
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 13:47:23 +0300

    feat: an extension for Result to conveniently convert an expected error to a Problem or success

---
+++ MeetingRoomsBooking/Api/Extensions/ResultToActionResultExtensions.cs
@@ -0,0 +1,68 @@
+﻿using MeetingRoomsBooking.Features.Abstractions.Common.Errors;
+using MeetingRoomsBooking.Features.Abstractions.Common.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingRoomsBooking.Api.Extensions
+{
+    public static class ResultToActionResultExtensions
+    {
+        public static ApiError Internal() => new("INTERNAL_SERVER_ERROR", "Internal server Error");
+
+        public static IActionResult ToActionResult<T>(
+            this ControllerBase controller,
+            Result<T> result)
+            =>
+            result.Status switch
+            {
+                ResultStatus.Ok => result.Data is null
+                ? controller.NoContent()
+                : controller.Ok(result.Data),
+
+                ResultStatus.Created => result.Data is null
+                ? controller.ToProblemDetails(Internal(), StatusCodes.Status500InternalServerError)
+                : controller.Created("", result.Data),
+
+                ResultStatus.BadRequest => controller.ToProblemDetails(
+                    result.Error ?? Internal(),
+                    StatusCodes.Status400BadRequest),
+
+                ResultStatus.Conflict => controller.ToProblemDetails(result.Error ?? Internal(),
+                    StatusCodes.Status409Conflict),
+
+                ResultStatus.NotFound => controller.ToProblemDetails(result.Error ?? Internal(),
+                    StatusCodes.Status404NotFound),
+
+                ResultStatus.Unauthorized => controller.ToProblemDetails(result.Error ?? Internal(),
+                    StatusCodes.Status401Unauthorized),
+
+                _ => controller.ToProblemDetails(Internal(), StatusCodes.Status500InternalServerError)
+            };
+
+
+        public static IActionResult ToProblemDetails(
+            this ControllerBase controller,
+            ApiError error,
+            int statusCode)
+        {
+            var problem = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = error.Message,
+                Detail = error.Message,
+                Type = error.Code
+            };
+
+            problem.Extensions["code"] = error.Code;
+            problem.Extensions["traceId"] = controller.HttpContext.TraceIdentifier;
+
+            if (error.Details is not null)
+                problem.Extensions["details"] = error.Details;
+
+            return new ObjectResult(problem)
+            {
+                StatusCode = statusCode,
+                ContentTypes = { "application/problem+json" }
+            };
+        }
+    }
+}
