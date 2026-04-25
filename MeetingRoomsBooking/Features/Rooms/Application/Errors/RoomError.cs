992e7e1082fc1bce924c9e82bf721943dee1e47c
Oleg Boodko <dafsas321@gmail.com>
2026-04-25 13:58:45 +0300

    feat: Added return of business errors from the application layer from Rooms

---
+++ MeetingRoomsBooking/Features/Rooms/Application/Errors/RoomError.cs
@@ -0,0 +1,10 @@
+﻿using MeetingRoomsBooking.Features.Abstractions.Common.Errors;
+
+namespace MeetingRoomsBooking.Features.Rooms.Application.Errors
+{
+    public static class RoomError
+    {
+        public static ApiError NameAlreadyExists(string value)
+            => new("ROOM_NAME_ALREADY_EXISTS", $"The room name is already exists: {value}.");
+    }
+}
