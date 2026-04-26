17cb71cea61f0248084e96568742c12a4c670dec
Oleg Boodko <dafsas321@gmail.com>
2026-04-26 13:20:10 +0300

    feat: Added a new Bookings repository and queries with separation of application and infrastructure layers

---
+++ MeetingRoomsBooking/Features/Bookings/Application/Abstractions/Queries/IBookingQueries.cs
@@ -0,0 +1,17 @@
+﻿using MeetingRoomsBooking.BuildingBlocks.Domain.Room.RoomId;
+using MeetingRoomsBooking.Features.Bookings.Domain.ValueObjects.TimeSlot;
+using MeetingRoomsBooking.Features.Rooms.Application.ReadModels;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomLocation;
+
+namespace MeetingRoomsBooking.Features.Bookings.Application.Abstractions.Queries
+{
+    public interface IBookingQueries
+    {
+        Task<bool> IsCanBooking(RoomId roomId, TimeSlot reqTime, CancellationToken ct);
+
+        Task<RoomReadModel?> GetById(
+            RoomId roomId,
+            RoomLocation location,
+            CancellationToken ct);
+    }
+}
---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Features/Bookings/Queries/EfBookingQueries.cs
@@ -0,0 +1,49 @@
+﻿using MeetingRoomsBooking.BuildingBlocks.Domain.Room.RoomId;
+using MeetingRoomsBooking.Features.Bookings.Application.Abstractions.Queries;
+using MeetingRoomsBooking.Features.Bookings.Domain.Enums;
+using MeetingRoomsBooking.Features.Bookings.Domain.ValueObjects.TimeSlot;
+using MeetingRoomsBooking.Features.Rooms.Application.ReadModels;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomLocation;
+using MeetingRoomsBooking.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Features.Bookings.Queries
+{
+    public class EfBookingQueries : IBookingQueries
+    {
+        private readonly BookingDbContext _db;
+
+        public EfBookingQueries(BookingDbContext db) => _db = db;
+
+        public async Task<bool> IsCanBooking(RoomId roomId, TimeSlot reqTime, CancellationToken ct)
+        {
+            return !await _db.BookingRequests
+                .AnyAsync(b => b.RoomId == roomId &&
+                b.Status == BookingStatus.Confirmed &&
+                b.TimeSlot.StartAtUtc < reqTime.EndAtUtc &&
+                b.TimeSlot.EndAtUtc > reqTime.StartAtUtc,
+                ct);
+        }
+
+
+        public async Task<RoomReadModel?> GetById(
+            RoomId roomId,
+            RoomLocation location,
+            CancellationToken ct)
+        {
+             return await _db.Rooms
+                .AsNoTracking()
+                .Where(r => r.Id == roomId)
+                .Select(r => new RoomReadModel
+                {
+                    Id = r.Id.Value,
+                    Name = r.Name.Value,
+                    Capacity = r.Capacity.Value,
+                    Location = r.Location.Value,
+                    IsActive = r.IsActive
+                })
+                .FirstOrDefaultAsync(ct);
+        }
+
+    }
+}
