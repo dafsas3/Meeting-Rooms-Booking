8f98ac85a4bda80c659ce8558469c1524d49a7f2
Oleg Boodko <dafsas321@gmail.com>
2026-04-26 13:22:43 +0300

    fix: Moving the Room repository and queries to the Persistence structure

---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Features/Rooms/Queries/EfRoomQueries.cs
@@ -0,0 +1,24 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Application.Abstractions.Queries;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomLocation;
+using MeetingRoomsBooking.Features.Rooms.Domain.ValueObjects.RoomName;
+using MeetingRoomsBooking.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Features.Rooms.Queries
+{
+    public class EfRoomQueries : IRoomQueries
+    {
+        private readonly BookingDbContext _db;
+
+        public EfRoomQueries(BookingDbContext db) => _db = db;
+
+
+        public async Task<bool> IsExistsByNameAndLocationAsync(
+            RoomName name,
+            RoomLocation location,
+            CancellationToken ct)
+            =>
+            await _db.Rooms
+            .AnyAsync(r => r.Name == name && r.Location == location, ct);
+    }
+}
---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Features/Rooms/Repositories/EfRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using MeetingRoomsBooking.Features.Rooms.Domain.Entities;
+using MeetingRoomsBooking.Features.Rooms.Application.Abstractions.Repositories;
+using MeetingRoomsBooking.Infrastructure.Persistence.Data;
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Features.Rooms.Repositories
+{
+    public sealed class EfRoomRepository : IRoomRepository
+    {
+        private readonly BookingDbContext _db;
+
+        public EfRoomRepository(BookingDbContext db) => _db = db;
+
+
+        public void Add(Room entity) => _db.Add(entity);
+    }
+}
