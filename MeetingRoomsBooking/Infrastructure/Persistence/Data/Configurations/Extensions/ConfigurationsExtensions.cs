a0c056daccf3c606d2af6bc06db143e7b062d750
Oleg Boodko <dafsas321@gmail.com>
2026-04-27 00:20:18 +0300

    feat: Added reusable value objects for EF conversion

--- MeetingRoomsBooking/Infrastructure/Persistence/Data/Configurations/Extensions/ConfigurationsExtensions.cs
+++ MeetingRoomsBooking/Infrastructure/Persistence/Data/Configurations/Extensions/ConfigurationsExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using MeetingRoomsBooking.BuildingBlocks.Domain.Room.RoomId;
+using MeetingRoomsBooking.BuildingBlocks.Domain.ValueObjects.IdempotencyKey;
+using MeetingRoomsBooking.Features.Bookings.Domain.Ids.BookingRequestId;
+using MeetingRoomsBooking.Features.Bookings.Domain.Ids.UserId;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MeetingRoomsBooking.Infrastructure.Persistence.Data.Configurations.Extensions
@@ -7,5 +10,17 @@ namespace MeetingRoomsBooking.Infrastructure.Persistence.Data.Configurations.Ext
     {
         public static PropertyBuilder<RoomId> HasRoomIdConversion(this PropertyBuilder<RoomId> property)
             => property.HasConversion(rId => rId.Value, value => RoomId.FromDatabase(value));
+
+        public static PropertyBuilder<IdempotencyKey> HasIdempotencyKeyConversion(
+            this PropertyBuilder<IdempotencyKey> property) =>
+            property.HasConversion(key => key.Value, value => IdempotencyKey.Create(value));
+
+        public static PropertyBuilder<BookingRequestId> HasBookingRequestIdConversion(
+            this PropertyBuilder<BookingRequestId> property) =>
+            property.HasConversion(id => id.Value, value => BookingRequestId.FromDatabase(value));
+
+        public static PropertyBuilder<EmployeeId> HasEmployeeIdConversion(
+            this PropertyBuilder<EmployeeId> property) =>
+            property.HasConversion(id => id.Value, value => EmployeeId.FromDatabase(value));
     }
 }
