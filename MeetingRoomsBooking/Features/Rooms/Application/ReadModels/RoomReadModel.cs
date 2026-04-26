8f986d969d97c43e6349ebbc676ed26afbf8dcce
Oleg Boodko <dafsas321@gmail.com>
2026-04-26 13:21:19 +0300

    feat: Added the Entity Room Reading Model when working with the database

---
+++ MeetingRoomsBooking/Features/Rooms/Application/ReadModels/RoomReadModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MeetingRoomsBooking.Features.Rooms.Application.ReadModels
+{
+    public sealed class RoomReadModel
+    {
+        public required int Id;
+        public required string Name;
+        public required int Capacity;
+        public required string Location;
+        public required bool IsActive;
+    }
+}
