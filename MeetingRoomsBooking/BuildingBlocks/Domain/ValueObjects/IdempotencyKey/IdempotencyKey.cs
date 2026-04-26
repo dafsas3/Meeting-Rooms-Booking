d3d36bed1ca2f0d7bcc9c737dc66bfebabf2af12
Oleg Boodko <dafsas321@gmail.com>
2026-04-27 00:28:20 +0300

    feat: Added Idempotency Key and error

---
+++ MeetingRoomsBooking/BuildingBlocks/Domain/ValueObjects/IdempotencyKey/IdempotencyKey.cs
@@ -0,0 +1,17 @@
+﻿namespace MeetingRoomsBooking.BuildingBlocks.Domain.ValueObjects.IdempotencyKey
+{
+    public sealed record IdempotencyKey
+    {
+        public Guid Value { get; }
+
+        private IdempotencyKey(Guid value) => Value = value;
+
+        public static IdempotencyKey Create(Guid value)
+        {
+            if (value == Guid.Empty)
+                throw InvalidIdempotencyKeyException.MustBeNotEmpty(value);
+
+            return new IdempotencyKey(value);
+        }
+    }
+}
---
+++ MeetingRoomsBooking/BuildingBlocks/Domain/ValueObjects/IdempotencyKey/InvalidIdempotencyKeyException.cs
@@ -0,0 +1,21 @@
+﻿using MeetingRoomsBooking.Features.Abstractions.Domain;
+
+namespace MeetingRoomsBooking.BuildingBlocks.Domain.ValueObjects.IdempotencyKey
+{
+    public sealed class InvalidIdempotencyKeyException : DomainException
+    {
+        private InvalidIdempotencyKeyException(Guid value, string reason) : base(
+            code: "INVALID_IDEMPOTENCY_KEY",
+            message: $"Incorrect idempotency key: {value}. Reason: {reason}.",
+            meta: new Dictionary<string, object?>
+            {
+                ["Value"] = value,
+                ["Reason"] = reason
+            })
+        { }
+
+
+        public static InvalidIdempotencyKeyException MustBeNotEmpty(Guid value) =>
+            new(value, "The idempotency key cannot by empty.");
+    }
+}
