67beb4b0e0df720e0ad2bdad5d83f43168db8f03
Oleg Boodko <dafsas321@gmail.com>
2026-04-27 19:06:38 +0300

    feat: Adding a new migration

---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Data/Migrations/20260427152925_AddXminConcurrencyToBookingRequest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddXminConcurrencyToBookingRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "xmin",
+                table: "BookingRequests",
+                type: "xid",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "xmin",
+                table: "BookingRequests");
+        }
+    }
+}
