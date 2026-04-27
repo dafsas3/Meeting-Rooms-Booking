a54dd61d61351f7b1e965a491571e29e70f1ba32
Oleg Boodko <dafsas321@gmail.com>
2026-04-27 12:07:19 +0300

    feat: Re-creating the database and a new migration to clean up the project and remove incorrect test data

---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Data/Migrations/20260426231850_InitCreate.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookingRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoomId = table.Column<int>(type: "integer", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IdempotencyKey = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TimeSlot_EndAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MeetingPurpose = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    Location = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookingHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BookingRequestId = table.Column<int>(type: "integer", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActorRole = table.Column<int>(type: "integer", nullable: false),
+                    FromStatus = table.Column<int>(type: "integer", nullable: false),
+                    ToStatus = table.Column<int>(type: "integer", nullable: false),
+                    Reason = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookingHistories_BookingRequests_BookingRequestId",
+                        column: x => x.BookingRequestId,
+                        principalTable: "BookingRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookingParticipants",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    BookingRequestId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingParticipants", x => new { x.BookingRequestId, x.Email });
+                    table.ForeignKey(
+                        name: "FK_BookingParticipants_BookingRequests_BookingRequestId",
+                        column: x => x.BookingRequestId,
+                        principalTable: "BookingRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingHistories_BookingRequestId",
+                table: "BookingHistories",
+                column: "BookingRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingHistories_BookingRequestId_CreatedAtUtc",
+                table: "BookingHistories",
+                columns: new[] { "BookingRequestId", "CreatedAtUtc" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingRequests_Employee_Status",
+                table: "BookingRequests",
+                columns: new[] { "EmployeeId", "Status" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingRequests_Room_Status_TimeSlot",
+                table: "BookingRequests",
+                columns: new[] { "RoomId", "Status" });
+
+            migrationBuilder.CreateIndex(
+                name: "UX_BookingRequests_IdempotencyKey",
+                table: "BookingRequests",
+                column: "IdempotencyKey",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UX_Rooms_Name_Location",
+                table: "Rooms",
+                columns: new[] { "Name", "Location" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookingHistories");
+
+            migrationBuilder.DropTable(
+                name: "BookingParticipants");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "BookingRequests");
+        }
+    }
+}
