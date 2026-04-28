c73d2ea43805139b8ddbbc4fdccac0bf937b6a59
Oleg Boodko <dafsas321@gmail.com>
2026-04-28 18:18:13 +0300

    feat: Add migrations. Update booking request relations

---
+++ MeetingRoomsBooking/Infrastructure/Persistence/Data/Migrations/20260428010947_UpdateBookingRequestRelations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetingRoomsBooking.Infrastructure.Persistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookingRequestRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeSlot_EndAtUtc",
+                table: "BookingRequests",
+                newName: "EndAtUtc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EndAtUtc",
+                table: "BookingRequests",
+                newName: "TimeSlot_EndAtUtc");
+        }
+    }
+}
